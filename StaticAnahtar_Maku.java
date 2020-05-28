7776717d705312389b59ac47f925ee346e7e1b6b
cankartal48 <66039760+cankartal48@users.noreply.github.com>
2020-05-28 14:49:56 +0300

    Add files via upload

---
+++ StaticAnahtar_Maku.java
@@ -0,0 +1,14 @@
+
+package staticanahtar_maku;
+
+public class StaticAnahtar_Maku {
+    
+    public static void main(String[] args) {
+        ogrenci Ogrenci1=new ogrenci();//Yeni bir öğrenci oluşturdum.
+        ogrenci Ogrenci2=new ogrenci();//2.öğrenciyi oluşturdum.
+        ogrenci Ogrenci3=new ogrenci();//3.öğrenciyi oluşturdum.
+        
+        System.out.println("Öğrenci sayısı : " + Ogrenci1.ogrenci_sayisi);
+        //Öğrenci sayısını ekrana yazdırdım.
+    }   
+}
---
+++ ogrenci.java
@@ -0,0 +1,10 @@
+
+package staticanahtar_maku;
+
+public class ogrenci {//ogrenci isimli bir class oluşturduk.
+    public static int ogrenci_sayisi=0;// ogrenci_sayisi'ni bellek 1 defa oluşturmuş oluyoruz.Değeri 0 ile başlasın.
+    
+    public ogrenci(){
+  ogrenci_sayisi++;//Her bir obje oluşturulduğunda ogrenci_sayisi'ni 1 arttırıcam.
+    }
+}
